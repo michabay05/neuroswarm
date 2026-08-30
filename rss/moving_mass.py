0c8ff1489a7c0a638484ff6330251ebf08476019
michabay05 <michabay05@gmail.com>
2026-08-30 16:51:07 -0400

    fix: `world is None` issue
    In the constructor, the world is not yet saved in the metric. Solution: don't refer to the world in the __init__

--- rss/moving_mass.py
+++ rss/moving_mass.py
@@ -6,8 +6,9 @@ class MovingMass(Aggregation):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.initial_centroid = self.center_of_mass()
+        self.centroids = []
 
     def _calculate(self):
         centroid = self.center_of_mass()
-        return super()._calculate() + np.linalg.norm(centroid - self.initial_centroid)
+        self.centroids.append(centroid)
+        return super()._calculate() + np.linalg.norm(centroid - self.centroids[0])
